49403cbbd49d674c426eac28027464fbe20fb7b2
Tristan Dyer <atdyer@ncsu.edu>
2019-03-20 09:12:03 -0400

    Expressions work

--- sparse/dist/impl/yale.py
+++ sparse/dist/impl/yale.py
@@ -22,9 +22,27 @@ class Yale:
 
         return 0 if i is None else v[i]
 
-    def multiply(self, v):
+    def multiply(self, vector):
 
-        pass
+        length = len(vector)
+        solution = [0] * length
+
+        for row in range(length):
+
+            a = self.IA[row] if row < len(self.IA) else None
+            b = self.IA[row+1] if row+1 < len(self.IA) else None
+
+            if a is None or b is None or a == b:
+                continue
+
+            j = self.JA[a:b]
+            v = self.A[a:b]
+
+            for col, val in zip(j, v):
+
+                solution[row] += vector[col]*val
+
+        return solution
 
     def update(self, row, col, value):
 
@@ -100,3 +118,43 @@ class Yale:
     def _z_to_z(self, row, col):
 
         pass
+
+    def _rep_inv(self):
+
+        inv = (
+            0 not in self.A
+            and all([value <= self.rows*self.cols for value in self.IA])
+            and all([value < self.cols for value in self.JA])
+            and self.IA[0] == 0
+            and self.IA[-1] == len(self.A)
+            and (True if len(self.IA) <= 1 else self.IA[-1] > self.IA[-2])
+            and len(self.A) == len(self.JA)
+            and len(self.A) <= self.rows*self.cols
+            and len(self.IA) <= self.rows + 1
+        )
+
+        if not inv:
+            return False
+
+        for i in range(len(self.IA)-1):
+            a = i
+            b = i+1
+            if b < a:
+                return False
+            if b-a > self.cols:
+                return False
+            vals = set(self.JA[a:b])
+            if len(vals) != b-a:
+                return False
+
+        return True
+
+
+t = Yale(2, 2)
+t.update(0, 0, 1)
+t.update(0, 1, 2)
+t.update(1, 0, 3)
+t.update(1, 1, 4)
+r = t.multiply([1, 1])
+print(r)
+
